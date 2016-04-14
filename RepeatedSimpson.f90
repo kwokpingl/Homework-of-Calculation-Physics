125159cd9c135d96a75648a75da47127b5f2aa9f
YongyiZhou <409228602@qq.com>
2016-04-14 18:24:22 +0800

    04/14/2016 18:24

--- RepeatedSimpson.f90
+++ RepeatedSimpson.f90
@@ -1,24 +1,27 @@
-real*8 function f(x)
-    implicit none
-    real*8 :: x
-    f = sin(x)
-    return
+real*8 function f(x)        ! Define the origin function f(x)
+        implicit none
+        real*8 :: x
+        f = sin(x)
+        return
 end function f
 
 program main
-    implicit none
-    integer i ,m
-    real*8 h ,a ,b ,result ,x1 ,x2
-    real*8,external::f
-    h=0.1
-    a=1
-    b=5
-    m=(b-a)/(2.0*h)
-    result = h/3.0*( f(a) + f(b) + f(a) )
-    do i=1,m-1
-        x1 = a+(2*i+1)*h
-        x2 = a+(2*i)*h
-        result = result + h/3.0* ( 2.0 * (2.0* f(x1) + f(x2) ) )
-    enddo
-    write(*,*) "The result is : ",result
+        implicit none
+        integer i ,m
+        real*8 h ,a ,b ,result ,x1 ,x2
+        real*8,external::f
+        write(*,*) "Repeated Simpson integration :"
+        ! Initialize the index
+        h=0.1d0
+        a=1.0d0
+        b=5.0d0
+        m=(b-a)/(2.0d0*h)
+        result = h/3.0d0*( f(a) - f(b) )
+        ! Calculate the integrate
+        do i=1,m
+                x1 = a+(2*i-1)*h
+                x2 = a+(2*i)*h
+                result = result + h/3.0d0* ( 2.0d0 * (2.0d0* f(x1) + f(x2) ) )
+        enddo
+        write(*,*) "The result is : ",result
 end program main
--- RepeatedTrapezoid.f90
+++ RepeatedTrapezoid.f90
@@ -1,25 +1,26 @@
-real*8 function f(x)
-    implicit none
-    real*8 :: x
-    f = sin(x)
-    return
+real*8 function f(x)            ! Define the origin function f(x)
+        implicit none
+        real*8 :: x
+        f = sin(x)
+        return
 end function f
 
 program main
-    implicit none
-    integer i ,n
-    real*8 h ,a ,b ,result ,xi
-    real*8,external::f
-    h=0.1
-    a=1.0
-    b=5.0
-    n=(b-a)/h
-    print *,n
-    result =h/2.0*( f(a) + f(b) )
-    xi = a
-    do i=1,n-1
+        implicit none
+        integer i ,n
+        real*8 h ,a ,b ,result ,xi
+        real*8,external::f
+        write(*,*) "Repeated Trapezoid integration :"
+        ! Initialize the index
+        h=0.1d0
+        a=1.0d0
+        b=5.0d0
+        n=(b-a)/h
+        result =h/2.0*( f(a) + f(b) )
+        ! Calculate the integrate
+        do i=1,n-1
         xi = a+i*h
-        result = result + h/2.0*2.0*f(xi)
-    enddo
-    write(*,*) "The result is : ",result
+        result = result + h*f(xi)
+        enddo
+        write(*,*) "The result is : ",result
 end program main
