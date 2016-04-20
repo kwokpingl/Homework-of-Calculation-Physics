94d5c3fe42c2ad0fd9e7d8a34ccecb1e2cb7b63f
YongyiZhou <409228602@qq.com>
2016-04-20 08:44:34 +0800

    04/20/2016 08:44

--- Drawmap.f90
+++ Drawmap.f90
@@ -7,64 +7,69 @@ program main
     !Calculate the Map
     do x=0,1800/20
         ! Get the X-axis value 
-        write(*,*) "Calculating the X-axis value",x
         ! Get f(x,0)
-        Map(x,0)=Cubic( Map(0,0),Map(600/20,0),Map(1200/20,0),Map(1800/20,0))
+        Map(x,0)=Cubic( x*20d0,0d0,600d0,1200d0,1800d0,&
+                                &Map(0,0),Map(600/20,0),Map(1200/20,0),Map(1800/20,0))
         ! Get f(x,500)
-        Map(x,500/20)=Cubic( Map(0,500/20),Map(600/20,500/20),Map(1200/20,500/20),Map(1800/20,500/20))
+        Map(x,500/20)=Cubic( x*20d0,0d0,600d0,1200d0,1800d0,&
+                                        &Map(0,500/20),Map(600/20,500/20),Map(1200/20,500/20),Map(1800/20,500/20))
         ! Get f(x,1000)
-        Map(x,1000/20)=Cubic( Map(0,1000/20),Map(600/20,1000/20),Map(1200/20,1000/20),Map(1800/20,1000/20))
+        Map(x,1000/20)=Cubic( x*20d0,0d0,600d0,1200d0,1800d0,&
+                                          &Map(0,1000/20),Map(600/20,1000/20),Map(1200/20,1000/20),Map(1800/20,1000/20))
         ! Get f(x,1500)
-        Map(x,1500/20)=Cubic( Map(0,1500/20),Map(600/20,1500/20),Map(1200/20,1500/20),Map(1800/20,1500/20))
+        Map(x,1500/20)=Cubic( x*20d0,0d0,600d0,1200d0,1800d0,&
+                                          &Map(0,1500/20),Map(600/20,1500/20),Map(1200/20,1500/20),Map(1800/20,1500/20))
         
         ! Get the Y-axis value
-        write(*,*) "Calculating the Y-axis value ,y="
         do y=0,1500/20
             ! Get f(x,y)
-            Map(x,y)=Cubic( Map(x,0),Map(x,500/20),Map(x,1000/20),Map(x,1500/20))
-            write(*,*) "x = " ,x*20 ,"y = " ,y*20
+            Map(x,y)=Cubic( y*20d0,0d0,500d0,1000d0,1500d0,Map(x,0),Map(x,500/20),Map(x,1000/20),Map(x,1500/20))
+            write(*,*) x,y,Map(x,y)
         enddo
     enddo
 
     ! Write the result
-    write(*,*) "write the result..."
+    write(*,*) "------------------------- All Calculation Finished -------------------------"
+    write(*,*) "Write the result into 'Map.txt'..."
     open(1,file='Map.txt')
     do i=0,1800/20
         do j=0,1500/20
-            write(1,*) i*20,Map(i,j*20)
+            !print *,i*20 ,j*20 ,Map(i,j)
+            write(1,*) i*20 ,j*20 ,Map(i,j)
         enddo
     enddo
     close(1)
+    write(*,*) "Succeed."
 end program main
 
 
 
-real*8 function Cubic(num0,num1,num2,num3)
+real*8 function Cubic(xi,x0,x1,x2,x3,num0,num1,num2,num3)
     implicit none
     integer i,j,k
-    real*8 :: num0 ,num1 ,num2 ,num3
+    real*8 :: num0 ,num1 ,num2 ,num3,x0,x1,x2,x3
     real*8 Y(0:3) ,X(0:3) ,m(0:3) ,M1(0:3) ,h(0:2)
     real*8 a(0:2) ,b(2) ,a1(0:2) ,b1(2)     !Define a(0) to default ,which is of no use in calculation ,while calculating Q(i,j) for QM=b
     real*8 Q(2,0:3) ,Q1(2,0:3)                 !Define Q(i,0) & Q(i,3) to default ,in case of over boundary
     real*8 xi ,result ,result1 ,alpha(0:3) ,beta(0:3)
-    m(0)    = -(1.0+25.0)**(-2)*2.0*(-5.0)
-    m(3)    = -(1.0+25.0)**(-2)*2.0*5.0
-    M1(0)  = 2.0*(1.0+25.0)**(-3)*4.0*25.0 - (1.0+25.0)**(-2) *8.0*(-5.0)
-    M1(3)  = 2.0*(1.0+25.0)**(-3)*4.0*25.0 - (1.0+25.0)**(-2) *8.0*(5.0)
-    write(*,*) "Cubic Spline Curve (Both San-Wan-Ju & San-Zhuan-Jiao) :"
-    write(*,*) "Reading the x and y..."
+    m(0)    = 0
+    m(3)    = 0
+    M1(0)  = 0
+    M1(3)  = 0
+    !write(*,*) "Cubic Spline Curve (Both San-Wan-Ju & San-Zhuan-Jiao) :"
+    !write(*,*) "Reading the x and y..."
     !Read data knew
     do i=0,3
-        X=(/0,1,2,3/)       !Read X,Y
+        X=(/x0,x1,x2,x3/)                       !Read X ,Y
         Y=(/num0,num1,num2,num3/)
     enddo
     !Calculate the h(i)
-    write(*,*) "Calculating the h(i)..."
+    !write(*,*) "Calculating the h(i)..."
     do i=0,2
         h(i) = X(i+1)-X(i)
     enddo
     !Calculate the a(i) & b(i)
-    write(*,*) "Calculating the a(i) & b(i)..."
+    !write(*,*) "Calculating the a(i) & b(i)..."
     do k=1,2
         ! San Zhuan Jiao Equation
         a(k) = h(k-1)/(h(k-1)+h(k))
@@ -79,7 +84,7 @@ real*8 function Cubic(num0,num1,num2,num3)
     b1(1)  = b1(1) - (1-a1(1))*M1(0)
     b1(2)= b1(2) - a1(2)*M1(3)
     !Build the matrix Q ,for Qm=b & QM=b
-    write(*,*) "Building the Q(i,i)..."
+    !write(*,*) "Building the Q(i,i)..."
     Q=0
     Q1=0
     do i=1,2
@@ -93,7 +98,7 @@ real*8 function Cubic(num0,num1,num2,num3)
 
     !Slove equations by Chasing Elimination
     !Build Triangular matrix Q
-    write(*,*) "Building the upper Triangular matrix..."
+    !write(*,*) "Building the upper Triangular matrix..."
     do i=1,1
             j=i+1
             Q(j,i:i+1) = Q(j,i:i+1) - Q(i,i:i+1)*Q(j,i)/Q(i,i)
@@ -104,13 +109,13 @@ real*8 function Cubic(num0,num1,num2,num3)
     !Slove Equations QM=b
     Q(2,3)=0
     Q1(2,3)=0
-    write(*,*) "Calculating the m(i) & M(i)..."
+    !write(*,*) "Calculating the m(i) & M(i)..."
     do i = 2 ,1 ,-1
             m(i)   = (b(i)  -(Q(i, i+1)   *m(i+1)))  /Q(i,i)
             M1(i) = (b1(i)-(Q1(i, i+1)*M1(i+1)))/Q1(i,i)
     enddo
 
-    write(*,*) "Calculating the S(x)..."
+    !write(*,*) "Calculating the S(x)..."
     result=0     ! Initialize result
     result1=0   ! Initialize result1
     !Calculate alpha & beta ,in addition to
@@ -128,7 +133,7 @@ real*8 function Cubic(num0,num1,num2,num3)
         endif
     enddo
     result=dot_product(Y(0:3),alpha(0:3)) + dot_product(m(0:3),beta(0:3))
-    write(*,*) "San-Zhuan-Jiao : ",result
+    !write(*,*) "San-Zhuan-Jiao : ",result
 
     !Calculate the S(i)
     !S(x)=M(k)*(X(k+1)-xi)**3/(6*h(k)) + M(k+1)*(xi-X(k))**3/h(k) + (Y(k)-M(k)*h(k)**2/6)*(X(k+1)-xi)/h(k) + (Y(k+1)-M(k+1)*h(k)**2/6)*(xi-X(k))/h(k)
@@ -142,8 +147,8 @@ real*8 function Cubic(num0,num1,num2,num3)
             exit
         endif
     enddo
-    write(*,*) "San-Wan-Ju",result1
-    write(*,*) "Succeed ."
+    !write(*,*) "San-Wan-Ju",result1
+    !write(*,*) "Succeed ."
     Cubic=result
     !Cubic=result1
     return
